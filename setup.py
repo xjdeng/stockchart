9a2b0b7ccd6b23134805a7f96589883940980097
xjdeng <xjd001@gmail.com>
2018-12-26 21:29:07 -0600

    Add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+#https://docs.python.org/2/distutils/setupscript.html
+#Info on including data files
+
+setup(name="stockchart",version='0.1',description='A minimalist program for\
+      plotting candlestick stock charts',\
+      author='Xiaojian Deng',author_email="xjd001@gmail.com",license='MIT',\
+      packages=['stockchart'],
+      url="https://github.com/xjdeng/stockchart",zip_safe=False)
